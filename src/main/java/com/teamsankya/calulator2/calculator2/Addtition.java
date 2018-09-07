baecaeaca41ce51c938e15cacfeb25b53ba4fd3a
nitishkr736 <nitishara.2010@gmail.com>
2018-09-07 12:06:34 +0530

    new project cal2

---
+++ src/main/java/com/teamsankya/calulator2/calculator2/Addtition.java
@@ -0,0 +1,8 @@
+package com.teamsankya.calulator2.calculator2;
+
+public class Addtition {
+	public static  int add (int a, int b) {
+		return a+b;
+	}
+
+}
---
+++ src/main/java/com/teamsankya/calulator2/calculator2/App.java
@@ -0,0 +1,13 @@
+package com.teamsankya.calulator2.calculator2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
